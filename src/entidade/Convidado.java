1a425189033b6ccbf370c925101bea99f8de78d6
mclairex <mariasousaclara2019@gmail.com>
2025-11-21 20:34:26 -0300

    Adiciona interfaces iniciais.

--- src/entidade/Convidado.java
+++ src/entidade/Convidado.java
@@ -1,28 +1,40 @@
  package entidade;
 
-import desconto.Desconto;
-import desconto.DescontoRegular;
-import desconto.DescontoVIP;
-
+ //Usar bibliotecas
+ 
     public class Convidado {
+        private int id;
         private String nome;
         private String tipo;
-        private Desconto estrategiaDesconto;
 
-        public Convidado(String nome, String tipo) {
+        public Convidado(int id, String nome, String tipo) {
+            this.id = id;
             this.nome = nome;
             this.tipo = tipo;
-            // Configuração da estratégia de desconto (DIP/OCP)
-            if (tipo.equalsIgnoreCase("VIP")) {
-                this.estrategiaDesconto = new DescontoVIP();
-            } else {
-                this.estrategiaDesconto = new DescontoRegular();
-            }
         }
 
-        public String getNome() { return nome; }
-        public String getTipo() { return tipo; }
-        public Desconto getEstrategiaDesconto() { return estrategiaDesconto; }
+        // Getters e Setters
+        public int getId() { return id; } 
+        public void setId(int id) { this.id = id; }
+        public String getNome() { return nome; } 
+        public void setNome(String nome) { this.nome = nome; }
+        public String getTipo() { return tipo; } 
+        public void setTipo(String tipo) { this.tipo = tipo; }  
+
+        //public Convidado(String nome, String tipo) {
+        //    this.nome = nome;
+        //    this.tipo = tipo;
+        //    // Configuração da estratégia de desconto (DIP/OCP)
+        //    if (tipo.equalsIgnoreCase("VIP")) {
+        //        this.estrategiaDesconto = new DescontoVIP();
+        //    } else {
+        //        this.estrategiaDesconto = new DescontoRegular();
+        //    }
+        //}
+//
+        //public String getNome() { return nome; }
+        //public String getTipo() { return tipo; }
+        //public Desconto getEstrategiaDesconto() { return estrategiaDesconto; }
     }
 }
 
---
+++ src/entidade/ConvidadoRegular.java
@@ -0,0 +1,20 @@
+package entidade;
+
+    public class ConvidadoRegular extends Convidado {
+        private int id;
+        private String nome;
+        private String tipo;
+
+        public ConvidadoRegular(int id, String nome, String tipo) {
+            super(id, nome);
+            super.setTipo("Regular");
+        }
+
+        // Getters e Setters
+        public int getId() { return id; } 
+        public void setId(int id) { this.id = id; }
+        public String getNome() { return nome; } 
+        public void setNome(String nome) { this.nome = nome; }
+        public String getTipo() { return tipo; } 
+        public void setTipo(String tipo) { this.tipo = tipo; }  
+    }
